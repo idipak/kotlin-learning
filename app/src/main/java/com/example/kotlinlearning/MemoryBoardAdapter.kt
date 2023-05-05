a6d2b2625bdbc766cf5160615769e2794f2d441b
Dipak <dipak_pd@outlook.com>
2023-05-05 08:40:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlinlearning/MemoryBoardAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinlearning
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoryBoardAdapter(private val context: Context, private  val numPieces: Int) :
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+}
